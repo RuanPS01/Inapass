b4852cc91b83d795f0dda679873857f3f731213f
Pedro Polez <pedro_hpr@hotmail.com>
2018-08-15 19:58:58 -0300

    Rota inicial arrumada

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,6 @@
+var path = require('path');
+
 module.exports.renderIndex =function(application, req, res){
-  res.render("index");
+  //Envia o arquivo HTML ao navegador na resposta
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
 }
