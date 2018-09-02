d66bda8e9468665f2004d2d3e766e64bf2e494b2
Ruan Patrick de Souza <ruansouza@gec.inatel.br>
2018-09-02 17:13:40 -0300

    Index Login HTML
    Primeira pagina ainda não interligadacom o backend.

--- config/server.js
+++ config/server.js
@@ -6,7 +6,6 @@ app.set('view engine', 'html');
 app.set('views', './app/views');
 consign()
   .include('app/routers')
-  //.then('config/dbConnection.js')
   .then('app/models')
   .then('app/controllers')
   .into(app);
