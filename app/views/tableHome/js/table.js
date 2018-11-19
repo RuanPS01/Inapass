d9890f449fafcc71fc288346dc587680b2a33459
RuanPS01 <30608019+RuanPS01@users.noreply.github.com>
2018-11-18 23:30:01 -0200

    GET First Config

--- app/views/tableHome/js/table.js
+++ app/views/tableHome/js/table.js
@@ -2,6 +2,10 @@ $('table').on('click', '.parent', function(){
     $(this).toggleClass('.child').nextUntil('.parent').slideToggle(30);
 });
 
+$(document).ready(function() {
+  console.log("Ready!");
+  //listTable();
+});
 /* String.prototype.hashCode = function() {
   var hash = 0, i, chr;
   if (this.length === 0) return hash;
@@ -13,6 +17,82 @@ $('table').on('click', '.parent', function(){
   return hash;
 }; */
 
+
+
+function listTable(){
+  //debugger;
+  var xmlhttp = new XMLHttpRequest();
+  var url = "http://localhost:3000/listEntry";
+  
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myArr = JSON.parse(xmlhttp.responseText);
+      console.log(myArr);
+      arrayListing(myArr);
+    }
+    };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function arrayListing(arr) {
+  var out = "";
+  console.log("Array: "+arr);
+  var i;
+  for(i = 0; i < arr.length; i++) {
+    var siteOrigem = arr[i].siteOrigem;
+    var nameUser = arr[i].nameUser;
+    var linkSite = arr[i].linkSite;
+    var senhaUser = arr[i].senhaUser;
+    var descricao = arr[i].descricao;
+
+    var table = document.getElementById("Table");
+
+    var rowCount = table.rows.length;
+    var newRow = table.insertRow(rowCount);
+    newRow.className = "parent";
+  
+    var cell0_SiteOrigem = newRow.insertCell(0);
+    cell0_SiteOrigem.innerHTML = '<td>'+siteOrigem+'</td>';
+    cell0_SiteOrigem.className = 'column1';
+  
+    var cell1_NomeUsuario = newRow.insertCell(1);
+    cell1_NomeUsuario.innerHTML = '<td>'+nameUser+'</td>';
+    cell1_NomeUsuario.className = 'column2';
+  
+    var cell2_Link = newRow.insertCell(2);
+    cell2_Link.innerHTML = '<td>'+linkSite+'</td>';
+    //cell2_Link.href = linkSite;
+    cell2_Link.className = 'column3';
+  
+    var cell3_Blank= newRow.insertCell(3);
+    cell3_Blank.innerHTML = '<td></td>';
+    cell3_Blank.className = 'column4';
+  
+    /////////////ROW CHILD///////////////////////////
+    var newRowCHILD = table.insertRow(rowCount+1);
+    newRowCHILD.className = "cchild child";
+    
+    var cell0_CHILD_Text = newRowCHILD.insertCell(0);
+    cell0_CHILD_Text.innerHTML = '<td>Senha:</td>';
+    cell0_CHILD_Text.className = 'column1';
+  
+    var cell1_CHILD_Senha = newRowCHILD.insertCell(1);
+    cell1_CHILD_Senha.innerHTML = '<td>'+senhaUser+'</td>';
+    cell1_CHILD_Senha.className = 'column2';
+  
+    var cell2_CHILD_Descr = newRowCHILD.insertCell(2);
+    cell2_CHILD_Descr.innerHTML = '<td>Obs: '+descricao+'</td>';
+    cell2_CHILD_Descr.className = 'column3';
+  
+    var cell2_CHILD_Button = newRowCHILD.insertCell(3);
+    cell2_CHILD_Button.innerHTML = '<td><button>Selecionar</button></td>';
+    cell2_CHILD_Button.setAttribute("onclick", "selectRegister()");
+    cell2_CHILD_Button.className = 'pillbutton';
+  }
+}
+
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
@@ -23,7 +103,6 @@ function openFormEdit() {
   }else{
     alert("Selecione um cadastro!");
   }
-  
 }
 
 function closeFormEdit() {
