72b548a3a18687ebc6fec5d322eb86caf2aff44e
Pedro Polez <pedro_hpr@hotmail.com>
2018-11-20 03:00:28 -0200

    Fixed backend and frontend doc update

--- backend/index.js
+++ backend/index.js
@@ -78,7 +78,6 @@ server.post('/updateEntry', function(req, res){
   axios.get('http://'+credentials.name+':'+credentials.password+'@'+bdAddr+'/passdb'+'/'+req.query.docId)
   .then(function (response) {
     recData._rev = response.data._rev;
-
     axios.put('http://'+credentials.name+':'+credentials.password+'@'+bdAddr+'/passdb'+'/'+response.data._id, recData)
     .then(function (response){
       console.log("[couchdb]Succesfully updated document!");
